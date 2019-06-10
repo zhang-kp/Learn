19c4d95b3ae65cb2807c563d22ab0b0ce402980c
pengpeng <zhangkp1006@163.com>
2019-06-10 14:21:13 +0800

    add EasyExcel

---
+++ easyexcel/src/test/java/com/zkp/easyexcel/model/Commodity.java
@@ -0,0 +1,25 @@
+//package com.zkp.easyexcel.model;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class Commodity {
+//
+////    private String c1;
+////    private String c1;
+////    private String c1;
+////    private String c1;
+////    private String c1;
+////    private String c1;
+////    private String c1;
+////    private String c1;
+////    private String c1;
+////    private String c1;
+////    private String c1;
+//}
---
+++ easyexcel/src/test/java/com/zkp/easyexcel/model/User.java
@@ -0,0 +1,18 @@
+package com.zkp.easyexcel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private String name;
+
+    private String password;
+
+    private Integer age;
+}
